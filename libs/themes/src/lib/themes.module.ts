eb9b5a343a121a502f9a758b693ff950ac06bb2e
Bram Borggreve <borggreve@gmail.com>
2020-05-29 12:24:29 -0500

    feat: implement themes

--- libs/themes/src/lib/themes.module.ts
+++ libs/themes/src/lib/themes.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { ThemePickerComponent } from './theme-picker.component';
+import { ThemesConfig } from './themes.config';
+import { ThemesService } from './themes.service';
+import { THEMES } from '../version';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ThemePickerComponent],
+  exports: [ThemePickerComponent],
 })
-export class ThemesModule {}
+export class ThemesModule {
+  public static forRoot(
+    config: ThemesConfig = { themes: THEMES }
+  ): ModuleWithProviders {
+    return {
+      ngModule: ThemesModule,
+      providers: [{ provide: ThemesConfig, useValue: config }, ThemesService],
+    };
+  }
+}
