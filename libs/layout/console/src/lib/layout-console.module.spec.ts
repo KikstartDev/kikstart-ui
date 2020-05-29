5a4b93feff70fc30e41ccc5af84d86c5c46d7a1e
Bram Borggreve <borggreve@gmail.com>
2020-05-29 11:27:08 -0500

    feat: create layout-console lib

--- libs/layout/console/src/lib/layout-console.module.spec.ts
+++ libs/layout/console/src/lib/layout-console.module.spec.ts
@@ -1,7 +1,7 @@
 import { async, TestBed } from '@angular/core/testing';
 import { LayoutConsoleModule } from './layout-console.module';
 
-describe('ConsoleModule', () => {
+describe('LayoutConsoleModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [LayoutConsoleModule],
