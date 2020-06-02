58c508759b512b9191dd9dcc0af1f1df56ce0a0b
Bram Borggreve <borggreve@gmail.com>
2020-06-02 16:02:23 -0500

    Release 1.1.0

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -3,24 +3,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
+import { routes } from './app.routes'
 import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(
-      [
-        { path: '', pathMatch: 'full', redirectTo: 'console' },
-        {
-          path: 'console',
-          loadChildren: () => import('./console/console.module').then((m) => m.ConsoleModule),
-        },
-      ],
-      { initialNavigation: 'enabled' },
-    ),
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
   providers: [],
   bootstrap: [AppComponent],
 })
